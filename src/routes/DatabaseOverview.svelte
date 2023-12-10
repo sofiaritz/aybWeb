92195e5e31d2c43c3b368a1c265a047e894ba29b
Sofía Aritz <sofi@sofiaritz.com>
2023-12-10 16:54:01 +0100

    Prettify

--- src/routes/DatabaseOverview.svelte
+++ src/routes/DatabaseOverview.svelte
@@ -10,22 +10,24 @@
 	<div class="flex w-full flex-col">
 		<h1 class="text-2xl">Congratulations, you've got your database set-up!</h1>
 		<div class="flex gap-6">
-			<div class="mt-6 w-full rounded border border-gray-300 dark:border-gray-800 p-6">
+			<div class="mt-6 w-full rounded border border-gray-300 p-6 dark:border-gray-800">
 				<h2 class="text-xl">Documentation</h2>
 				<span class="mt-1 block text-gray-500 dark:text-gray-400"
 					>Check out the link below for more information about ayb</span
 				>
 				<a class="mt-1 block" href="https://git.sofiaritz.com/sofia/wip">ayb.host/docs</a>
 			</div>
-			<div class="mt-6 w-full rounded border border-gray-300 dark:border-gray-800 p-6">
+			<div class="mt-6 w-full rounded border border-gray-300 p-6 dark:border-gray-800">
 				<h2 class="text-xl">Getting help</h2>
-				<span class="mt-1 block text-gray-500 dark:text-gray-400">Contact the instance owner for support</span>
+				<span class="mt-1 block text-gray-500 dark:text-gray-400"
+					>Contact the instance owner for support</span
+				>
 				<a class="mt-1 block" href="https://git.sofiaritz.com/sofia/wip">ayb.host/support</a
 				>
 			</div>
 		</div>
 		<div class="flex gap-6">
-			<div class="mt-6 w-full rounded border border-gray-300 dark:border-gray-800 p-6">
+			<div class="mt-6 w-full rounded border border-gray-300 p-6 dark:border-gray-800">
 				<h2 class="text-xl">Usage limits</h2>
 				<span class="mt-1 block text-gray-500 dark:text-gray-400"
 					>You are not subject to usage limits right now</span
